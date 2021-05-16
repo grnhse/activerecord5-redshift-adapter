79f7b37c7ebcd4119945516d46f005faf57c83b1
Quentin Rousseau <contact@quent.in>
2021-05-16 15:55:28 -0700

    Fixes for Rails >= 6.1

--- lib/active_record/connection_adapters/redshift/column.rb
+++ lib/active_record/connection_adapters/redshift/column.rb
@@ -4,8 +4,18 @@ module ActiveRecord
       delegate :oid, :fmod, to: :sql_type_metadata
       attr_accessor :identity
 
-      def initialize(name, default, sql_type_metadata, null = true, default_function = nil)
-        super name, default, sql_type_metadata, null, default_function
+      if ActiveRecord::VERSION::MAJOR >= 6 && ActiveRecord::VERSION::MINOR >= 1
+        # Required for Rails 6.1, see https://github.com/rails/rails/pull/41756
+        mattr_reader :array, default: false
+        alias :array? :array
+
+        def initialize(name, default, sql_type_metadata, null = true, default_function = nil, **)
+          super name, default, sql_type_metadata, null, default_function
+        end
+      else
+        def initialize(name, default, sql_type_metadata, null = true, default_function = nil)
+          super name, default, sql_type_metadata, null, default_function
+        end
       end
     end
   end
--- lib/active_record/connection_adapters/redshift/schema_statements.rb
+++ lib/active_record/connection_adapters/redshift/schema_statements.rb
@@ -3,6 +3,45 @@
 module ActiveRecord
   module ConnectionAdapters
     module Redshift
+
+      if ActiveRecord::VERSION::MAJOR >= 6 && ActiveRecord::VERSION::MINOR >= 1
+        class SchemaCreation < SchemaCreation
+          private
+
+          def visit_ColumnDefinition(o)
+            o.sql_type = type_to_sql(o.type, limit: o.limit, precision: o.precision, scale: o.scale)
+            super
+          end
+
+          def add_column_options!(sql, options)
+            column = options.fetch(:column) { return super }
+            if column.type == :uuid && options[:default] =~ /\(\)/
+              sql << " DEFAULT #{options[:default]}"
+            else
+              super
+            end
+          end
+        end
+      else
+        class SchemaCreation < AbstractAdapter::SchemaCreation
+          private
+
+          def visit_ColumnDefinition(o)
+            o.sql_type = type_to_sql(o.type, limit: o.limit, precision: o.precision, scale: o.scale)
+            super
+          end
+
+          def add_column_options!(sql, options)
+            column = options.fetch(:column) { return super }
+            if column.type == :uuid && options[:default] =~ /\(\)/
+              sql << " DEFAULT #{options[:default]}"
+            else
+              super
+            end
+          end
+        end
+      end
+
       module SchemaStatements
         # Drops the database specified on the +name+ attribute
         # and creates it again using the provided +options+.
